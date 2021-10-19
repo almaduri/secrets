11d38a21c80e13ebf4bee5082bde956a481dd2bc
Mohammad Ali Al Maduri <ali.mdrw@gmail.com>
2021-10-19 17:02:03 +0800

    Add submit route and update the secrets

--- app.js
+++ app.js
@@ -26,7 +26,8 @@ mongoose.connect('mongodb://localhost:27017/userDB')
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-  googleId: String
+  googleId: String,
+  secret: String
 })
 
 userSchema.plugin(passportLocalMongoose)
@@ -84,11 +85,40 @@ app.get('/register', (req, res) => {
 })
 
 app.get('/secrets', (req, res) => {
+  User.find({secret: {$ne: null}}, (err, foundUsers) => {
+    if(err) {
+      console.log(err)
+    } else {
+      if(foundUsers) {
+        res.render('secrets', {usersWithSecrets: foundUsers})
+      }
+    }
+  })
+})
+
+app.get('/submit', (req, res) => {
   if(req.isAuthenticated()) {
-    res.render('secrets')
+    res.render('submit')
   } else {
-    res.redirect('/login')
+    red.redirect('/login')
   }
+})
+
+app.post('/submit', (req, res) => {
+  const submittedSecret = req.body.secret
+  console.log(req.user.id)
+  User.findById(req.user.id, (err, foundUser) => {
+    if(err) {
+      console.log(err)
+    } else {
+      if(foundUser) {
+        foundUser.secret = submittedSecret
+        foundUser.save(() => {
+          res.redirect('/secrets')
+        })
+      }
+    }
+  })
 })
 
 app.get('/logout', (req, res) => {
